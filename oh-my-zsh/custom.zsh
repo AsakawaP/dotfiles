9472549cb626114fb780b37497151a641bd1def1
浅川 <iDOLM@STER.LIVE>
2024-07-25 15:35:49 +0800

    oh my zsh

--- oh-my-zsh/custom.zsh
+++ oh-my-zsh/custom.zsh
@@ -8,20 +8,25 @@ alias omzcustom="nano $ZSH_CUSTOM/custom.zsh"
 # sudo
 alias sudo="sudo "
 
-# cli tools
-alias eza1="exa -aglT --header --icons -L 1"
-alias ezat="exa -aglT --header --icons"
-alias ezal="exa -aglT --header --icons -L"
+# debian snap
+# export PATH=$PATH:/snap/bin
 
 # morden unix
 alias bat="batcat"
 alias fd="fdfind"
 
-# mainland china
-# unalias gg
-# compdef _precommand gg
+# cli tools
+alias ezat="exa -aglT --header --icons"
+alias ezal="exa -aglT --header --icons -L"
+alias eza1="exa -aglT --header --icons -L 1"
+alias eza2="exa -aglT --header --icons -L 2"
+alias dud="du -ch -d"
 
 # container
 # alias docker="sudo docker"
 # alias watchtower="sudo docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower"
 # alias rclone="sudo docker run --rm -it -v ~/rclone/config:/config/rclone --user $(id -u):$(id -g) -v ~/rclone/data:/data rclone/rclone"
+
+# mainland china
+# unalias gg
+# compdef _precommand gg
